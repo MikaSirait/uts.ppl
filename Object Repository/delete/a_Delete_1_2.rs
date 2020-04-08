a8e0e068efe4c89b437263554bba33b975f66ddd
MikaSirait <mika.sirait@si.ukdw.ac.id>
2020-04-08 12:46:17 +0700

    first commit crud

---
+++ Object Repository/delete/a_Delete_1_2.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Delete_1_2</name>
+   <tag></tag>
+   <elementGuidId>4bf0116b-6acb-4fbc-8ab6-4b24e1fa0947</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[@onclick=&quot;return confirm('Are you sure you want to delete?')&quot;])[9]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>http://[::1]/ProjectSimpleCI-master/index.php/news/delete/9</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onclick</name>
+      <type>Main</type>
+      <value>return confirm('Are you sure you want to delete?')</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Delete</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/table[1]/tbody[1]/tr[10]/td[3]/a[3]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>(//a[@onclick=&quot;return confirm('Are you sure you want to delete?')&quot;])[9]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Delete')])[9]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Edit'])[9]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='View'])[9]/following::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='tessss'])[1]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[@href='http://[::1]/ProjectSimpleCI-master/index.php/news/delete/9']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tr[10]/td[3]/a[3]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/edit/body_Simple CRUDHome  Add NewsNews archive _456ffc.rs
@@ -0,0 +1,211 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_Simple CRUDHome  Add NewsNews archive _456ffc</name>
+   <tag></tag>
+   <elementGuidId>86f7e5d6-0394-43ed-9041-86cb1abbbf67</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+		Simple CRUD
+		Home | Add News
+News archive
+ 
+
+    
+        Title
+        Content
+        Action
+    
+        
+            1st test
+            apakah
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            5th test
+            apa
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            2nd test
+            siapakah
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            3rd test
+            apa
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            4th test
+            dimana
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            6th test
+            halo
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            7th test
+            hai
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            8th test
+            aloha
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            9th test
+            lalala
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            10th test
+            tessss
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            12th test
+            jaja
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            14th test
+            hahahoho
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            11th test
+            lala lili land
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            15th test
+            i
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            13th test
+            lilola
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            13th test
+            lilola
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            new
+            new new
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+
+Copyright © 2016
+
+
+/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
